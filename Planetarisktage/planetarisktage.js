ae6a52d83a71935d825146cfe1d1546068c939bd
Klara919 <eaa25Kmbj@students.eaaa.dk>
2025-10-07 23:47:23 +0200

    the end

---
+++ Planetarisktage/planetarisktage.js
@@ -0,0 +1,19 @@
+const button = document.getElementById('openButton');
+const transitionVideo = document.getElementById('transitionVideo');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // Vis og fade-in videoen
+    transitionVideo.style.display = 'block';
+    setTimeout(() => {
+        transitionVideo.style.opacity = 1;
+    }, 10); // lille delay for at trigger CSS transition
+
+    transitionVideo.play();
+
+    // Når videoen er færdig
+    transitionVideo.addEventListener('ended', () => {
+        window.location.href = 'whitedwarf.html'; // Ret til din ønskede side
+    });
+});
